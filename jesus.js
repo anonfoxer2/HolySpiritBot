cfe7134c096c4d3aa4107f2976b93cde5038ac1f
anonfoxer <kevinandlife@gmail.com>
2022-08-05 00:39:56 -0500

    initial commit

---
+++ jesus.js
@@ -0,0 +1,34 @@
+//a lot of the boilerplate for this bot is the same as Flume. The more interesting code will be in the commands.
+
+const Discord = require('discord.js'); //create client
+const commando = require('discord.js-commando');
+const {TOKEN} = require('./secret');
+const {OWNERID} = require('./secret');
+
+
+const bot = new commando.Client({
+    commandPrefix: 'god>',
+    
+    owner: OWNERID,
+
+    disableEveryone: true
+});
+
+
+bot.on('ready', ( ) => {
+    bot.user.setActivity('Im quite hungry!');
+    console.log("FlumeBot Online! v2.5.5 ");
+})
+
+
+
+/* allocate commands */
+registerCommands();
+
+function registerCommands() { //registers all commands. 
+    bot.registry.registerGroup('prank', 'Prank');
+    bot.registry.registerCommandsIn(__dirname + "/commands"); //commands found in <dicrec>\commands.
+}
+
+
+bot.login(TOKEN);
